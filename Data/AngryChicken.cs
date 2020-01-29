11776ad0372be3e3a60b3944eb359b0005621d92
William Brown <wbrown2492@gmail.com>
2020-01-29 14:55:33 -0600

    Complete Angry Chicken

--- Data/AngryChicken.cs
+++ Data/AngryChicken.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
+    /// <summary>
+    /// A class representing the angry chicken entree
+    /// </summary>
     public class AngryChicken
     {
         /// <summary>
