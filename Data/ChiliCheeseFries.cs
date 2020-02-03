96b2e7402b91886ee167e6c29f93f8574364d740
wabrown <wabno1@yahoo.com>
2020-02-03 15:18:29 -0600

    Chili Cheese

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class ChiliCheeseFries : Side
+    {
+
+
+        public override double Price => throw new NotImplementedException();
+
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 433;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Large:
+                        return 610;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+}
+}
