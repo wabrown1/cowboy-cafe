bdec8e5334f9aa48d4a95946da66b0adbe8897dd
wabrown <wabrown@F1092-02-2090>
2020-02-26 14:48:59 -0600

    Order Tests

---
+++ Data/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class Order
+    {
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public double Subtotal => 0;
+
+        public void Add(IOrderItem item) { }
+
+        public void Remove(IOrderItem item) { }
+    }
+}
---
+++ DataTests/OrderTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CowboyCafe.Data;
+using Xunit;
+using System.Linq;
+
+namespace CowboyCafe.DataTests
+{
+    class MockOrderItem : IOrderItem
+    {
+        public double Price { get; set; }
+
+        public List<string> SpecialInstructions { get; set; }
+    }
+    public class OrderTests
+    {
+        //adding something to the order shold
+        //have it appear in the Items property
+        [Fact]
+        public void AddedIOrderItemsAppearInItemsProperty()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.Contains(item, order.Items);            
+        }
+
+        [Fact]
+        public void RemovedOrderItemDoesNotAppearInItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            order.Remove(item);
+            Assert.DoesNotContain(item, order.Items);
+        }
+
+        [Theory]
+        [InlineData(new double[] {})]
+        [InlineData(new double[] { 0 })]
+        [InlineData(new double[] { 10, 15, 18 })]
+        [InlineData(new double[] { 20, -4, 3.6, 8 })]
+        [InlineData(new double[] { -100, -5})]
+        public void SubtotalShouldBeTheSumOfOrderItemPrices(double[] prices)
+        {
+            var order = new Order();
+            double total = 0;
+            foreach (var price in prices)
+            {
+                total += price;
+                order.Add(new MockOrderItem()
+                {
+                    Price = price
+                });
+            }
+            Assert.Equal(total, order.Subtotal);
+        }
+
+        [Fact]
+        public void ItemsShouldContainOlnlyAddedItems()
+        {
+            var items = new IOrderItem[]
+            {
+                new MockOrderItem() {Price = 3 },
+                new MockOrderItem() {Price = 4 },
+                new MockOrderItem() {Price = 7 }
+            };
+            var order = new Order();
+            foreach(var item in items)
+            {
+                order.Add(item);
+            }
+            Assert.Equal(items.Length, order.Items.Count());
+            foreach(var item in items)
+            {
+                Assert.Contains(item, order.Items);
+            }
+        }
+
+        //removing something from the order should
+        //remove it from the Items property
+
+        //get the price - needs the be right
+        //(for the items we've added)
+
+
+    }
+}
